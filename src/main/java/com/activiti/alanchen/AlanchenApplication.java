367036faf83ff7d7bea11b4cf4b0fbe93787e3d4
AlanChen <chenyan900520@126.com>
2020-09-15 14:04:51 +0800

    first init

---
+++ src/main/java/com/activiti/alanchen/AlanchenApplication.java
@@ -0,0 +1,13 @@
+package com.activiti.alanchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlanchenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlanchenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/activiti/alanchen/AlanchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activiti.alanchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlanchenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
